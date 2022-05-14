48374acbe73192e07610f26025117a15ed5acc52
terakawakohei <trivover@icloud.com>
2022-05-14 20:26:11 +0900

    add lint

---
+++ src/components/ImageConverter.tsx
@@ -0,0 +1,17 @@
+import React,{useState} from 'react'
+
+type DisplayImage={
+    url:string
+    size:string
+}
+
+const ImageConverter=()=>{
+    return (
+        <div>
+            <input type="file" /> 
+                    <ul>hghg</ul>
+                    </div>
+
+    )
+}
+export default ImageConverter
